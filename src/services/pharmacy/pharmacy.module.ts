f67da0503f1fd1a6c7e9de1886d0551f99293b9c
Aadesh Santosh Bhujbal <aadeshbhujbal1@gmail.com>
2026-03-12 12:04:15 +0530

    feat: introduce PharmacyModule and integrate it into the main AppModule.

--- src/services/pharmacy/pharmacy.module.ts
+++ src/services/pharmacy/pharmacy.module.ts
@@ -4,9 +4,10 @@ import { PharmacyService } from './services/pharmacy.service';
 import { RbacModule } from '@core/rbac/rbac.module';
 import { ClinicModule } from '@services/clinic/clinic.module';
 import { PaymentModule } from '@payment/payment.module';
+import { EventsModule } from '@infrastructure/events/events.module';
 
 @Module({
-  imports: [RbacModule, ClinicModule, PaymentModule],
+  imports: [RbacModule, ClinicModule, PaymentModule, EventsModule],
   controllers: [PharmacyController],
   providers: [PharmacyService],
   exports: [PharmacyService],
